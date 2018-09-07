c4539c1d6a5c9be7d31dcd8bdf886ffa3ba09f29
Nils Ekenbäck <ekenback@gmail.com>
2018-09-07 11:59:14 +0200

    Code cleanup and removed rounding of double

--- A1_HMM/src/utils/MatrixOperations.java
+++ A1_HMM/src/utils/MatrixOperations.java
@@ -1,23 +1,16 @@
-import java.math.BigDecimal;
+package utils;
 
 public class MatrixOperations {
-    public int length;
-    public int height;
-    private double[][] matrix;
-
-    public MatrixOperations(int length, int height) {
-        this.length = length;
-        this.height = height;
-        this.matrix = new double[height][length];
-    }
-
 
     public static double[][] multiply(double[][] A, double[][] B) {
         int ACol = A[0].length;
         int BRow = B.length;
-        if(ACol != BRow) return null;
+
+        if(ACol != BRow ) return null;
+
         int ARow = A.length;
         int BCol = B[0].length;
+
         double[][] result = new double[ARow][BCol];
         for(int i = 0; i < ARow; i++) {
             for(int j = 0; j < BCol; j++) {
@@ -30,9 +23,9 @@ public class MatrixOperations {
     }
 
     // For debugging
-    public static void printMatrix(String varName, double[][] matrix) {
-        if (varName.length() > 0) {
-            System.out.println(varName);
+    public static void printMatrix(double[][] matrix, String... varName) {
+        if (varName.length > 0) {
+            System.out.println(varName[0]);
         }
         for (double[] m : matrix) {
             for (double var : m) {
@@ -41,5 +34,14 @@ public class MatrixOperations {
             System.out.println();
         }
         System.out.println();
+    }
+
+    public static double[][] calculateEmission(double[][] A, double[][] B, double[][] pi) {
+        double[][] a;
+
+        a = MatrixOperations.multiply(pi, A);
+        if (a == null) return null;
+
+        return MatrixOperations.multiply(a, B);
     }
 }
