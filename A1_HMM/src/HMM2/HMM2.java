ed0c04718f990386b216207f3e95bd11d9bb667f
Nils Ekenbäck <ekenback@gmail.com>
2018-09-19 15:25:48 +0200

    New getAnswer method

--- A1_HMM/src/HMM2/HMM2.java
+++ A1_HMM/src/HMM2/HMM2.java
@@ -5,6 +5,7 @@ import utils.Matrices;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class HMM2 {
     private static double[][] A;
@@ -14,15 +15,17 @@ public class HMM2 {
 
     public HMM2() {
         double[][] delta = estimateSequenceStateVector();
-
+        //Matrices.printMatrix(delta);
         int[] sequence = new int[delta.length];
         int idx;
         for (int i=delta.length-1; i >= 0; i--) {
             idx = getStateSequence(delta[i]);
             sequence[i] = idx;
         }
+        Stack<Integer> res = getAnswer(delta);
 
-        for (int s : sequence) System.out.print(s + " ");
+        for (int s : res) System.out.print(s + " ");
+        System.out.println();
     }
 
     public int getStateSequence(double[] delta) {
@@ -35,12 +38,25 @@ public class HMM2 {
         return idx;
     }
 
-    public double[][] estimateSequenceStateVector() {
+    public Stack<Integer> getAnswer(double[][] matrix) {
+        Stack<Integer> result = new Stack<>();
+        //int[] result = new int[matrix.length];
+        for (int row=0; row < matrix.length; row++) {
+            int idx = 0;
+            for (int col=0; col < matrix[0].length; col++) {
+                idx = matrix[idx][col] < matrix[row][col] ? col : idx;
+            }
+            result.push(idx);
+            //result[row] = idx;
+        }
+        return result;
+    }
 
+    public double[][] estimateSequenceStateVector() {
         double[][] b_i;
-        double[][] delta_i = new double[pi.length][pi[0].length];
+        double[][] delta_i;
         double[][] deltaMatrix = new double[A.length][A[0].length];
-        double[][] resultMatrix = new double[emissions.length][A[0].length];
+        double[][] deltaSequence = new double[emissions.length][A[0].length];
         for (int i=0; i < emissions.length; i++) {
             b_i = Matrices.getColumn(B, emissions[i]);
 
@@ -48,11 +64,12 @@ public class HMM2 {
                 // Get first delta vector
                 delta_i = Matrices.multiplyEntrywise(pi, b_i);
             } else {
-                // delta_i vector per row * b_i per col * a_ij
+                // previous delta_i vector per row * b_i per col * a_ij
                 int idx = 0;
+                delta_i = Matrices.getColumn(Matrices.transpose(deltaSequence), i-1);
                 for (int m=0; m < deltaMatrix.length; m++) {
                     for (int n=0; n < deltaMatrix[0].length; n++) {
-                        deltaMatrix[n][m] = A[n][m] * b_i[n][0] * delta_i[idx][0];
+                        deltaMatrix[m][n] = A[n][m] * b_i[idx][0] * delta_i[n][0];
                     }
                     idx += 1;
                 }
@@ -65,10 +82,10 @@ public class HMM2 {
             }
 
             // Get the max in each row to create the new delta vector
-            resultMatrix[i] = Matrices.transpose(delta_i)[0];
+            deltaSequence[i] = Matrices.transpose(delta_i)[0];
         }
 
-        return resultMatrix;
+        return deltaSequence;
     }
 
 
@@ -86,12 +103,9 @@ public class HMM2 {
         Scanner sc = new Scanner(new File(filepath));
         //Scanner sc = new Scanner(System.in);
 
-        // Need transposed A and B matrix
         int m;
         int n;
 
-
-        // Transposed A matrix
         m = sc.nextInt();
         n = sc.nextInt();
         A = new double[m][n];
