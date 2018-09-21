e6a0ffa47db28edc9eb701df9bcc016e0373d90b
Nils Ekenbäck <ekenback@gmail.com>
2018-09-21 12:05:24 +0200

    Refactored variable names for easier reading

--- A1_HMM/src/HMM2/HMM2.java
+++ A1_HMM/src/HMM2/HMM2.java
@@ -22,27 +22,27 @@ public class HMM2 {
 
 
     public int[] estimateSequenceStateVector() {
-        double[] b_t = observationMatrix[emissions[0]]; // First observation
+        double[] o_t = observationMatrix[emissions[0]]; // First observation
         int numOfEmissions = emissions.length;
         double[][] deltaMatrix = new double[transitionMatrix.length][numOfEmissions]; // Matrix for delta probabilities
         int[][] deltaIdxMatrix = new int[transitionMatrix.length][numOfEmissions]; // matrix for states
 
         // Get first delta vector
         for (int i = 0; i < transitionMatrix.length; i++) {
-            deltaMatrix[i][0] = pi[0][i] * b_t[i];
+            deltaMatrix[i][0] = pi[0][i] * o_t[i];
         }
 
         // Get delta vectors and states for t: 1-n
-        for (int t=1; t < emissions.length; t++) {
-            // Get observations at time t
-            b_t = observationMatrix[emissions[t]];
+        for (int t=1; t < numOfEmissions; t++) {
+            // Get observation o at time t
+            o_t = observationMatrix[emissions[t]];
 
-            // previous delta_i vector per row * b_t per col * a_nm
+            // previous delta_i vector per row * o_t per col * a_nm
             for (int m = 0; m < transitionMatrix.length; m++) {
                 int stateMax = -1;
                 double probabilityMax = -1;
                 for (int n = 0; n < transitionMatrix.length; n++) {
-                    double res = deltaMatrix[n][t-1] * b_t[m] * transitionMatrix[n][m];
+                    double res = deltaMatrix[n][t-1] * o_t[m] * transitionMatrix[n][m];
                     if (probabilityMax < res) {
                         probabilityMax = res;
                         stateMax = n;
@@ -57,7 +57,7 @@ public class HMM2 {
         double probabilityMax = -1;
         int stateMax = -1;
         for (int m = 0; m < transitionMatrix.length; m++) {
-            if (deltaMatrix[m][emissions.length-1] > probabilityMax) {
+            if (deltaMatrix[m][numOfEmissions-1] > probabilityMax) {
                 stateMax = m;
                 probabilityMax = deltaMatrix[m][numOfEmissions-1];
             }
@@ -66,7 +66,7 @@ public class HMM2 {
         // Get the state sequence
         int[] sequence = new int[numOfEmissions];
         sequence[numOfEmissions-1] = stateMax;
-        for (int i=emissions.length-1; i > 0; i--) {
+        for (int i=numOfEmissions-1; i > 0; i--) {
             sequence[i-1] = deltaIdxMatrix[sequence[i]][i];
         }
 
--- A1_HMM/src/HMM3/HMM3.java
+++
@@ -1,8 +0,0 @@
-package HMM3;
-
-public class HMM3 {
-
-    public static void main(String[] args) {
-
-    }
-}
