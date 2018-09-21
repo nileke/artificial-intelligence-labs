729d4783e8af0b0dbdae55227c5e93e8ec5778e1
Nils Ekenbäck <ekenback@gmail.com>
2018-09-21 14:48:56 +0200

    Added gammaPass method

--- A1_HMM/src/HMM3/HMM3.java
+++ A1_HMM/src/HMM3/HMM3.java
@@ -3,11 +3,19 @@ package HMM3;
 // import for printing
 import utils.Matrices;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class HMM3 {
     double[][] transitionMatrix;
     double[][] observationMatrix;
     double[][] pi;
     int[] emissions;
+
+    public HMM3() {
+
+    }
 
     public double[][] alphaPass(double[][] transitionMatrix,
                                 double[][] observationMatrix,
@@ -46,58 +54,29 @@ public class HMM3 {
                          double[][] pi,
                          int[] emissions) {
 
-        double[] o_t = observationMatrix[emissions[0]]; // First observation
         int numOfEmissions = emissions.length;
         double[][] deltaMatrix = new double[transitionMatrix.length][numOfEmissions]; // Matrix for delta probabilities
-        //int[][] deltaIdxMatrix = new int[transitionMatrix.length][numOfEmissions]; // matrix for states
 
         // Get first delta vector
         for (int i=0; i < transitionMatrix.length; i++) {
-            deltaMatrix[i][0] = pi[0][i] * o_t[i];
+            deltaMatrix[i][0] = pi[0][i] * observationMatrix[i][0];
         }
 
         // Get delta vectors and states for t: 1-n
         for (int t=1; t < numOfEmissions; t++) {
-            // Get observation o at time t
-            o_t = observationMatrix[emissions[t]];
-
-            // previous delta_i vector per row * o_t per col * a_nm
+            // previous delta_i vector per row * o_t per col * a_ji
             // then assign max value to the time step vector
             for (int i = 0; i < transitionMatrix.length; i++) {
-                //int stateMax = -1;
                 double probabilityMax = -1;
                 for (int j=0; j < transitionMatrix.length; j++) {
-                    double res = deltaMatrix[j][t-1] * o_t[i] * transitionMatrix[j][i];
+                    double res = deltaMatrix[j][t-1] * observationMatrix[i][t] * transitionMatrix[j][i];
                     if (probabilityMax < res) {
                         probabilityMax = res;
-                        //stateMax = j;
                     }
                 }
                 deltaMatrix[i][t] = probabilityMax;
-                //deltaIdxMatrix[i][t] = stateMax;
             }
         }
-
-        // Look for the highest value at the last time step t
-        /*
-        double probabilityMax = -1;
-        int stateMax = -1;
-        for (int m = 0; m < transitionMatrix.length; m++) {
-            if (deltaMatrix[m][numOfEmissions-1] > probabilityMax) {
-                stateMax = m;
-                probabilityMax = deltaMatrix[m][numOfEmissions-1];
-            }
-        }
-        */
-
-        // Get the state sequence
-        /*
-        int[] sequence = new int[numOfEmissions];
-        sequence[numOfEmissions-1] = stateMax;
-        for (int i=numOfEmissions-1; i > 0; i--) {
-            sequence[i-1] = deltaIdxMatrix[sequence[i]][i];
-        }
-        */
 
         return deltaMatrix;
 
@@ -108,9 +87,88 @@ public class HMM3 {
                           double[][] pi,
                           int[] emissions) {
 
+        double[][] gamma;
+        double[][] diGamma;
+
+        int numStates = transitionMatrix.length;
+        int numObservations = observationMatrix[0].length;
+
+        double[][] alphaPass = alphaPass(transitionMatrix, observationMatrix, pi, emissions);
+        double[][] betaPass = betaPass(transitionMatrix, observationMatrix, pi, emissions);
+        gamma = new double[numStates][numObservations];
+        diGamma = new double[numStates][numStates];
+
+        int timesteps = emissions.length;
+
+        for (int t=0; t < timesteps-1; t++) {
+            // Calculate the denominator
+            int denom = 0;
+            for (int i=0; i < numStates; i++) {
+                for (int j=0; j < numStates; j++) {
+                    denom += alphaPass[i][t] * transitionMatrix[i][j] * observationMatrix[j][t+1] * betaPass[j][t+1];
+                }
+            }
+            // Calculate gamma and diGamma w/ denominator
+            for (int i=0; i < numStates; i++) {
+                for (int j=0; j < numStates; j++) {
+                    diGamma[i][j] = (
+                            alphaPass[i][t]
+                            * transitionMatrix[i][j]
+                            * observationMatrix[j][t+1]
+                            * betaPass[j][t+1]
+                            ) / denom;
+
+                    gamma[i][t] += diGamma[i][j];
+                }
+            }
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        String filepath1 = "./A1_HMM/resources/hmm4_01.in";
+        String filepath2 = "./A1_HMM/resources/hmm4_02.in";
+        String filepath3 = "./A1_HMM/resources/hmm4_03.in";
+        Scanner sc = new Scanner(new File(filepath1));
 
+        double[][] transitionMatrix;
+        double[][] observationMatrix;
+        double[][] pi;
+        int[] emissions;
+
+        int m;
+        int n;
+
+        m = sc.nextInt();
+        n = sc.nextInt();
+        transitionMatrix = new double[m][n];
+        for (int i=0; i < m; i++) {
+            for (int j=0; j < n; j++) {
+                transitionMatrix[i][j] = sc.nextDouble();
+            }
+        }
+
+        m = sc.nextInt();
+        n = sc.nextInt();
+        observationMatrix = new double[m][n];
+        for (int i=0; i < m; i++) {
+            for (int j=0; j < n; j++) {
+                observationMatrix[i][j] = sc.nextDouble();
+            }
+        }
+
+        m = sc.nextInt();
+        n = sc.nextInt();
+        pi = new double[m][n];
+        for (int i=0; i < m; i++) {
+            for (int j=0; j < n; j++) {
+                pi[i][j] = sc.nextDouble();
+            }
+        }
+
+        n = sc.nextInt();
+        emissions = new int[n];
+        for (int i=0; i < n; i++) {
+            emissions[i] = sc.nextInt();
+        }
     }
 }
