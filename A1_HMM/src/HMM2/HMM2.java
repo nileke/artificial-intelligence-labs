071f6e6356b4798bc525db1b3e04d47c05e1a781
Nils Ekenbäck <ekenback@gmail.com>
2018-09-21 12:02:51 +0200

    Refactored variable names for easier reading

--- A1_HMM/src/HMM2/HMM2.java
+++ A1_HMM/src/HMM2/HMM2.java
@@ -1,73 +1,73 @@
 package HMM2;
 
 // Imports for prints
-import utils.Matrices;
-import java.util.Arrays;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class HMM2 {
-    private static double[][] A;
-    private static double[][] B;
+    private static double[][] transitionMatrix;
+    private static double[][] observationMatrix;
     private static double[][] pi;
     private static int[] emissions;
 
     public HMM2() {
         int[] stateSequence = estimateSequenceStateVector();
-
-        for (int s : stateSequence) System.out.print(s + " ");
-        System.out.println();
+        
+        StringBuilder output = new StringBuilder();
+        for (int s : stateSequence) output.append(s).append(" ");
+        System.out.println(output.toString());
     }
 
 
     public int[] estimateSequenceStateVector() {
-        double[] b_t = B[emissions[0]]; // First observation
-        double[][] deltaMatrix = new double[A.length][emissions.length]; // Matrix for delta probabilities
-        int[][] sequenceMatrix = new int[A[0].length][emissions.length]; // matrix for states
+        double[] b_t = observationMatrix[emissions[0]]; // First observation
+        int numOfEmissions = emissions.length;
+        double[][] deltaMatrix = new double[transitionMatrix.length][numOfEmissions]; // Matrix for delta probabilities
+        int[][] deltaIdxMatrix = new int[transitionMatrix.length][numOfEmissions]; // matrix for states
 
         // Get first delta vector
-        for (int i=0; i < A.length; i++) {
+        for (int i = 0; i < transitionMatrix.length; i++) {
             deltaMatrix[i][0] = pi[0][i] * b_t[i];
         }
 
         // Get delta vectors and states for t: 1-n
         for (int t=1; t < emissions.length; t++) {
             // Get observations at time t
-            b_t = B[emissions[t]];
+            b_t = observationMatrix[emissions[t]];
 
             // previous delta_i vector per row * b_t per col * a_nm
-            for (int m=0; m < A.length; m++) {
+            for (int m = 0; m < transitionMatrix.length; m++) {
                 int stateMax = -1;
                 double probabilityMax = -1;
-                for (int n=0; n < A.length; n++) {
-                    double res = deltaMatrix[n][t-1] * b_t[m] * A[n][m];
+                for (int n = 0; n < transitionMatrix.length; n++) {
+                    double res = deltaMatrix[n][t-1] * b_t[m] * transitionMatrix[n][m];
                     if (probabilityMax < res) {
                         probabilityMax = res;
                         stateMax = n;
                     }
                 }
                 deltaMatrix[m][t] = probabilityMax;
-                sequenceMatrix[m][t] = stateMax;
+                deltaIdxMatrix[m][t] = stateMax;
             }
         }
 
         // Look for the highest value at the last time step t
         double probabilityMax = -1;
         int stateMax = -1;
-        for (int m=0; m < A.length; m++) {
+        for (int m = 0; m < transitionMatrix.length; m++) {
             if (deltaMatrix[m][emissions.length-1] > probabilityMax) {
                 stateMax = m;
-                probabilityMax = deltaMatrix[m][emissions.length-1];
+                probabilityMax = deltaMatrix[m][numOfEmissions-1];
             }
         }
 
         // Get the state sequence
-        int[] sequence = new int[emissions.length];
-        sequence[emissions.length-1] = stateMax;
+        int[] sequence = new int[numOfEmissions];
+        sequence[numOfEmissions-1] = stateMax;
         for (int i=emissions.length-1; i > 0; i--) {
-            sequence[i-1] = sequenceMatrix[sequence[i]][i];
+            sequence[i-1] = deltaIdxMatrix[sequence[i]][i];
         }
 
         return sequence;
@@ -86,20 +86,20 @@ public class HMM2 {
 
         m = sc.nextInt();
         n = sc.nextInt();
-        A = new double[m][n];
+        transitionMatrix = new double[m][n];
         for (int i=0; i < m; i++) {
             for (int j=0; j < n; j++) {
-                A[i][j] = sc.nextDouble();
+                transitionMatrix[i][j] = sc.nextDouble();
             }
         }
 
-        // Transposed B so that every column is a sub-array
+        // Transposed observationMatrix so that every column is a sub-array
         m = sc.nextInt();
         n = sc.nextInt();
-        B = new double[n][m];
+        observationMatrix = new double[n][m];
         for (int i=0; i < m; i++) {
             for (int j=0; j < n; j++) {
-                B[j][i] = sc.nextDouble();
+                observationMatrix[j][i] = sc.nextDouble();
             }
         }
 
@@ -119,8 +119,8 @@ public class HMM2 {
         }
 
 /*
-        Matrices.printMatrix(A, "A matrix");
-        Matrices.printMatrix(B, "B matrix");
+        Matrices.printMatrix(transitionMatrix, "transitionMatrix matrix");
+        Matrices.printMatrix(observationMatrix, "observationMatrix matrix");
         Matrices.printMatrix(pi, "pi vector");
         System.out.println("=======================");
 */
