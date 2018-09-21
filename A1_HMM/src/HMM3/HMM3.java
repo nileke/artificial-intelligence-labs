72e70ac453fb20c1ca7ecb4c15f9b61bba7b105c
Nils Ekenbäck <ekenback@gmail.com>
2018-09-21 17:44:49 +0200

    Added re-estimate methods for transition, observation and pi

--- A1_HMM/src/HMM3/HMM3.java
+++ A1_HMM/src/HMM3/HMM3.java
@@ -8,12 +8,28 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class HMM3 {
-    double[][] transitionMatrix;
-    double[][] observationMatrix;
-    double[][] pi;
+    double[][] initialTransitionMatrix;
+    double[][] initialObservationMatrix;
+    double[][] initialPi;
     int[] emissions;
 
-    public HMM3() {
+    public HMM3(double[][] transitionMatrix,
+                double[][] observationMatrix,
+                double[][] pi,
+                int[] emissions) {
+        this.initialTransitionMatrix = transitionMatrix;
+        this.initialObservationMatrix = observationMatrix;
+        this.initialPi = pi;
+        this.emissions = emissions;
+
+    }
+
+    public void compute(double[][] transitionMatrix,
+                        double[][] observationMatrix,
+                        double[][] pi,
+                        int[] emissions) {
+
+        this.gammaPass(transitionMatrix, observationMatrix, pi, emissions);
 
     }
 
@@ -89,26 +105,32 @@ public class HMM3 {
 
         double[][] gamma;
         double[][] diGamma;
+        double[][] tempDiGammaSum;
 
         int numStates = transitionMatrix.length;
         int numObservations = observationMatrix[0].length;
 
+        // Initialize lambda, gamma and diGamma
         double[][] alphaPass = alphaPass(transitionMatrix, observationMatrix, pi, emissions);
         double[][] betaPass = betaPass(transitionMatrix, observationMatrix, pi, emissions);
         gamma = new double[numStates][numObservations];
         diGamma = new double[numStates][numStates];
 
+        // Sum of all diGamma matrices
+        tempDiGammaSum = new double[numStates][numStates];
+
         int timesteps = emissions.length;
 
         for (int t=0; t < timesteps-1; t++) {
-            // Calculate the denominator
+            // Calculate the denominator for gamma and diGamma calculation
             int denom = 0;
             for (int i=0; i < numStates; i++) {
                 for (int j=0; j < numStates; j++) {
                     denom += alphaPass[i][t] * transitionMatrix[i][j] * observationMatrix[j][t+1] * betaPass[j][t+1];
                 }
             }
-            // Calculate gamma and diGamma w/ denominator
+
+            // Calculate gamma and diGamma
             for (int i=0; i < numStates; i++) {
                 for (int j=0; j < numStates; j++) {
                     diGamma[i][j] = (
@@ -119,9 +141,54 @@ public class HMM3 {
                             ) / denom;
 
                     gamma[i][t] += diGamma[i][j];
+                    tempDiGammaSum[i][j] += diGamma[i][j];
                 }
             }
         }
+    }
+
+    public double[][] reEstimatePi(double[][] gamma) {
+        double[][] result = new double[gamma.length][1];
+        for (int i=0; i < gamma.length; i++) {
+            result[i][0] = gamma[i][0];
+        }
+        return result;
+    }
+
+    public double[][] reEstimateTransitionMatrix(double[][] diGammaSum, double[][] gamma, int timesteps) {
+        double[][] result = new double[diGammaSum.length][diGammaSum.length];
+        int denom;
+        for (int i=0; i < diGammaSum.length; i++) {
+            for (int j=0; j < diGammaSum.length; j++) {
+                denom = 0;
+                for (int t=0; t < timesteps; t++) {
+                    denom += gamma[i][t];
+                }
+                result[i][j] = diGammaSum[i][j] / denom;
+            }
+        }
+
+        return result;
+    }
+
+    public double[][] reEstimateObservationMatrix(double[][] gamma, int timesteps) {
+
+        double[][] result = new double[gamma.length][gamma[0].length];
+
+        double numer;
+        double denom;
+        for (int i=0; i < gamma.length; i++) {
+            for (int j=0; j < gamma[0].length; j++) {
+                numer = 0;
+                denom = 0;
+                for (int t=0; t < timesteps; t++) {
+                    if (t == j) numer += gamma[i][t];
+                    denom += gamma[i][t];
+                }
+                result[i][j] = numer / denom;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -170,5 +237,7 @@ public class HMM3 {
         for (int i=0; i < n; i++) {
             emissions[i] = sc.nextInt();
         }
+
+        new HMM3(transitionMatrix, observationMatrix, pi, emissions);
     }
 }
